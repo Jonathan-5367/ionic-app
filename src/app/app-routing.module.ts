b2c725e24166dee3cd80d2b4935cfe571114b37a
Jonathan-5367 <otakumtb@gmail.com>
2026-05-07 18:28:25 -0400

    Updates

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'info-personal',
+    loadChildren: () => import('./info-personal/info-personal.module').then( m => m.InfoPersonalPageModule)
+  },
+  {
+    path: 'contactos',
+    loadChildren: () => import('./contactos/contactos.module').then( m => m.ContactosPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/info-personal/info-personal.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-personal',
+  templateUrl: './info-personal.page.html',
+  styleUrls: ['./info-personal.page.scss'],
+  standalone: false,
+})
+export class InfoPersonalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
